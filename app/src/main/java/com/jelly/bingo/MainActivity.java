ff226c399382dc9883a0cca8d19a595546898de7
jellyMac <jellyfish6.cop04g@nctu.edu.tw>
2022-08-22 21:43:52 +0200

    Sign in

--- app/src/main/java/com/jelly/bingo/MainActivity.java
+++ app/src/main/java/com/jelly/bingo/MainActivity.java
@@ -1,17 +1,54 @@
 package com.jelly.bingo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    protected void onStart() {
+        FirebaseAuth.getInstance().addAuthStateListener(this);
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        FirebaseAuth.getInstance().removeAuthStateListener(this);
+        super.onStop();
+    }
+
+    @Override
+    public void onAuthStateChanged(@NonNull FirebaseAuth auth) {
+        if(auth.getCurrentUser() != null){
+            Log.d(TAG, "onAuthStateChanged: "+auth.getCurrentUser());
+
+        }else{
+            Log.d(TAG, "onAuthStateChanged: ");
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                    new AuthUI.IdpConfig.GoogleBuilder().build(),
+                    new AuthUI.IdpConfig.AnonymousBuilder().build()
+            ))
+            //.setIsSmartLockEnabled(false)
+            .build(),RC_SIGN_IN);
+
+        }
     }
 }
