beb8dc6b8537145d773caf4081f87a3123022805
jellyMac <jellyfish6.cop04g@nctu.edu.tw>
2022-08-28 01:31:25 +0200

    customerize NumberButton and show in gridLayout

--- app/src/main/java/com/jelly/bingo/BingoActivity.java
+++ app/src/main/java/com/jelly/bingo/BingoActivity.java
@@ -1,19 +1,105 @@
 package com.jelly.bingo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class BingoActivity extends AppCompatActivity {
     public String TAG = BingoActivity.class.getSimpleName();
+    private FirebaseRecyclerAdapter<Boolean, BallHolder> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bingo);
         String roomId = getIntent().getStringExtra("ROOM_ID");
-        boolean isCreator = getIntent().getBooleanExtra("IS_CREATOR",false);
-        Log.d(TAG, "onCreate: "+roomId+"/"+isCreator);
+        boolean isCreator = getIntent().getBooleanExtra("IS_CREATOR", false);
+        Log.d(TAG, "onCreate: " + roomId + "/" + isCreator);
+
+        if (isCreator) {
+            for (int i = 0; i < 25; i++) {
+                FirebaseDatabase.getInstance().getReference("rooms")
+                        .child(roomId)
+                        .child("numbers")
+                        .child(String.valueOf(i + 1))
+                        .setValue(false);
+            }
+        }
+        List<NumberButton> buttons = new ArrayList<>();
+        for (int i = 0; i < 25; i++) {
+            NumberButton button = new NumberButton(this);
+            button.setNumber(i+1);
+            buttons.add(button);
+        }
+        Collections.shuffle(buttons);
+
+        RecyclerView recycler = findViewById(R.id.game_recycler);
+        recycler.setHasFixedSize(true);
+        recycler.setLayoutManager(new GridLayoutManager(this, 5));
+
+        Query query = FirebaseDatabase.getInstance().getReference("rooms")
+                .child(roomId)
+                .child("numbers")
+                .orderByKey();
+        FirebaseRecyclerOptions<Boolean> options = new FirebaseRecyclerOptions.Builder<Boolean>()
+                .setQuery(query, Boolean.class)
+                .build();
+        adapter = new FirebaseRecyclerAdapter<Boolean, BallHolder>(options) {
+            @NonNull
+            @Override
+            public BallHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(BingoActivity.this).inflate(R.layout.single_button,parent,false);
+                return new BallHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull BallHolder holder, int position, @NonNull Boolean model) {
+                Log.d(TAG, "onBindViewHolder: "+position);
+                holder.button.setEnabled(!model);
+                holder.button.setText(String.valueOf(buttons.get(position).getNumber()));
+            }
+        };
+        recycler.setAdapter(adapter);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+
+    class BallHolder extends RecyclerView.ViewHolder {
+
+        NumberButton button;
+
+        public BallHolder(@NonNull View itemView) {
+            super(itemView);
+            button = itemView.findViewById(R.id.button);
+        }
     }
 }
---
+++ app/src/main/java/com/jelly/bingo/NumberButton.java
@@ -0,0 +1,48 @@
+package com.jelly.bingo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class NumberButton extends androidx.appcompat.widget.AppCompatButton {
+    int number;
+    boolean picked;
+    int pos;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+
+
+    public int getPos() {
+        return pos;
+    }
+
+    public void setPos(int pos) {
+        this.pos = pos;
+    }
+
+    public boolean isPicked() {
+        return picked;
+    }
+
+    public void setPicked(boolean picked) {
+        this.picked = picked;
+    }
+
+    public NumberButton(@NonNull Context context) {
+        super(context);
+    }
+
+    public NumberButton(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
